3bff7987a42c5c0388662c98dcac7d0a98f7e397
Trevor Cooper <tcooper@rockylinux.org>
2025-11-27 08:10:35 -0800

    add desktop_launch_terminal

--- lib/utils.pm
+++ lib/utils.pm
@@ -8,7 +8,7 @@ use Exporter;
 use feature "switch";
 use lockapi;
 use testapi;
-our @EXPORT = qw/run_with_error_check type_safely type_very_safely desktop_vt boot_to_login_screen console_login console_switch_layout desktop_switch_layout console_loadkeys_us do_bootloader boot_decrypt check_release menu_launch_type repo_setup cleanup_workaround_repo console_initial_setup handle_welcome_screen gnome_initial_setup anaconda_create_user check_desktop download_modularity_tests quit_firefox advisory_get_installed_packages advisory_check_nonmatching_packages start_with_launcher quit_with_shortcut lo_dismiss_tip disable_firefox_studies select_rescue_mode copy_devcdrom_as_isofile get_release_number get_version_major get_code_name check_left_bar check_top_bar check_prerelease check_version spell_version_number _assert_and_click is_branched rec_log click_unwanted_notifications repos_mirrorlist register_application get_registered_applications solidify_wallpaper/;
+our @EXPORT = qw/run_with_error_check type_safely type_very_safely desktop_vt boot_to_login_screen console_login console_switch_layout desktop_switch_layout console_loadkeys_us do_bootloader boot_decrypt check_release menu_launch_type repo_setup cleanup_workaround_repo console_initial_setup handle_welcome_screen gnome_initial_setup anaconda_create_user check_desktop download_modularity_tests quit_firefox advisory_get_installed_packages advisory_check_nonmatching_packages start_with_launcher quit_with_shortcut lo_dismiss_tip disable_firefox_studies select_rescue_mode copy_devcdrom_as_isofile get_release_number get_version_major get_code_name check_left_bar check_top_bar check_prerelease check_version spell_version_number _assert_and_click is_branched rec_log click_unwanted_notifications repos_mirrorlist register_application desktop_launch_terminal get_registered_applications solidify_wallpaper/;
 
 # We introduce this global variable to hold the list of applications that have
 # registered during the apps_startstop_test when they have sucessfully run.
@@ -1336,6 +1336,26 @@ sub register_application {
     my $application = shift;
     push(@application_list, $application);
     print("APPLICATION REGISTERED: $application \n");
+}
+
+# launch a terminal from a desktop, using the most efficient/reliable
+# approach (not appropriate if we really need to test launching it a
+# specific way)
+# Check, that the application has started.
+sub desktop_launch_terminal {
+    my $desktop = get_var("DESKTOP");
+    if ($desktop eq "i3") {
+        send_key "alt-ret";
+    }
+    elsif ($desktop eq "kde") {
+        send_key "ctrl-alt-t";
+    }
+    else {
+        my $string = 'terminal';
+        $string = 'ter;inql' if (get_var("LANGUAGE") eq 'french');
+        menu_launch_type($string, checkstart => 0);
+        assert_screen("apps_run_terminal");
+    }
 }
 
 # The KDE desktop tests are very difficult to maintain, because the transparency
